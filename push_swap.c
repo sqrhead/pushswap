80689a518396c44bcab2e892dd2dd3dd1fb5b7f0
sqrhead <fshelna98@gmail.com>
2026-01-04 22:18:19 +0100

    get_len_of_this_fucking_shit

--- push_swap.c
+++ push_swap.c
@@ -1,19 +1,30 @@
 #include "push_swap.h"
 
-
+void	log_tab(int *tab)
+{
+	while (*tab)
+		printf("%i ",*tab);
+}
 int main(int ac, char **av)
 {
 	int inpcheck;
+	int	*temp_stack;
 
 	if (ac < 2)
 		return (0);
 	if (ac == 2)
 	{
 		inpcheck = check_input(av[1]);
+
 		if (inpcheck == -1)
 		{
 			write(1,"Error\n",ft_strlen("Error\n"));
+			return (0);
 		}
+		temp_stack = (int *)malloc(sizeof(int) * count_input_elements(av[1]));
+		if (!temp_stack)
+			return (0);
+
 	}
 	else
 	{
--- utilities.c
+++ utilities.c
@@ -48,18 +48,24 @@ int	count_input_elements(char *str)
 		if (ft_isdigit(str[i]) || ft_issign(str[i]))
 		{
 			nele ++;
-			while (str[i] || ft_isdigit(str[i]))
+			while (ft_isdigit(str[i]))
 				i ++;
 		}
 		i ++;
 	}
+	return (nele);
 }
 
 int	parse_single_input(char *str,int *tab)
 {
     static int	index = 0;
     long		num;
+	int			n_elements;
 
+
+	if (!str)
+		return (1);
+	n_elements = count_input_elements(str);
     while (str[index] && (!ft_issign(str[index]) || !ft_isdigit(str[index])))
         index ++;
 	if (!str[index])
@@ -68,6 +74,9 @@ int	parse_single_input(char *str,int *tab)
 	if (num > INT_MAX || num < INT_MIN)
 		return (1);
 
+	n_elements = n_elements - index - 1;
+	tab[n_elements - 1] = num;
+	return (0);
 
 }
 
@@ -75,8 +84,11 @@ int	parse_mul_input(char *str, int *tab)
 {
 	int		index;
 	long	num;
+	int		n_elements;
+
 	if (!str)(
 		return (1);
+	n_elements = count_input_elements(str);
     while (str[index] && (!ft_issign(str[index]) || !ft_isdigit(str[index])))
 		index ++;
 	if (!str[index])
@@ -84,4 +96,23 @@ int	parse_mul_input(char *str, int *tab)
 	num = ft_atol(str[index]);
 	if (num > INT_MAX || num < INT_MIN)
 		return (1);
+	tab[n_elements - 1] = num;
+	n_elements --;
+}
+
+int	check_double(int *tab)
+{
+	int	curr;
+	int	index;
+
+
+	if (!tab)
+		return (1);
+
+	index = 0;
+	curr = tab[index];
+	while (tab[index])
+	{
+
+	}
 }
