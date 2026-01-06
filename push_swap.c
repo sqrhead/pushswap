83b7849f32318933ced977bb28ad017bc2e5501a
sqrhead <fshelna98@gmail.com>
2026-01-07 00:20:25 +0100

    mod

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sqrhead <sqrhead@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fshelna <fshelna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 08:54:39 by fshelna           #+#    #+#             */
-/*   Updated: 2026/01/06 20:45:37 by sqrhead          ###   ########.fr       */
+/*   Updated: 2026/01/06 22:56:11 by sqrhead          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,41 @@ void log_str(char **str)
 		str ++;
 	}
 }
-
-// void bubble_sort(int *tab, int size)
-// {
-// 	int index = 0;
-// 	int temp = 0;
-
-// 	while (index < size)
-// 	{
-
-// 	}
-// }
-
+//  negative numbers problem
+void bubble_sort(long *tab, int size)
+{
+	int 	i;
+	int	j;
+	long	temp;
+	i = 0;
+	j = 0;
+	while (i < size - 1)
+	{
+		while (j < size - i - 1)
+		{
+			if(tab[j + 1] && tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j ++;
+		}
+		i ++;
+	}
+}
 int main(int ac, char **av)
 {
 	long	*temp_stack;
-
+	if (ac < 2) // No Input
+	{
+		printf("Error: no elements passed\n");
+		return (0);
+	}
+	if (check_input(av[1]) == 1) // Input not valid 
+	{
+		return (0);
+	}
 	int n_elements = count_input_elements(av[1]);
 	printf("elements number : %i\n",n_elements);
 	temp_stack = (long *)malloc(sizeof(long) * n_elements);
@@ -62,10 +81,20 @@ int main(int ac, char **av)
 	while (div[index])
 	{
 		temp_stack[index] = ft_atol(div[index]);
+		if (temp_stack[index] > INT_MAX || temp_stack[index] < INT_MIN)
+		{
+			printf("Error: OVERFLOW || UNDERFLOW\n");
+			free_pp(div);
+			free(temp_stack);
+			return (0);
+		}
 		index ++;
 	}
-	log_tab(temp_stack,n_elements);
+	log_tab(temp_stack, n_elements);
 
+	// sort here && check for duplicates
+	bubble_sort(temp_stack, n_elements);
+	log_tab(temp_stack, n_elements);
 	free_pp(div);
 	free(temp_stack);
 }
