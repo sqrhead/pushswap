113da22588517017684eed5d9739c5389a16f301
sqrhead <fshelna98@gmail.com>
2026-01-18 19:56:28 +0100

    damn

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# PUSHSWAP
+𝘛𝘩𝘪𝘴 𝘱𝘳𝘰𝘫𝘦𝘤𝘵 𝘩𝘢𝘴 𝘣𝘦𝘦𝘯 𝘤𝘳𝘦𝘢𝘵𝘦𝘥 𝘢𝘴 𝘱𝘢𝘳𝘵 𝘰𝘧 𝘵𝘩𝘦 42 𝘤𝘶𝘳𝘳𝘪𝘤𝘶𝘭𝘶𝘮 𝘣𝘺 <𝘧𝘴𝘩𝘦𝘭𝘯𝘢>
 
+#Description
+## Project Description
+- A string or multiple containing integers numbers are given as input
+  With two stacks and a set of commands provided by the subject you need to sort it
+## Brief Overview
+- I checked if the input is valid
+- Converted and parsed in the Stack A
+- Divided the numbers in Chunks
+- Moved the chunks in B from the smallest
+- Moved the chunks in A from the biggest
+- And while doing this i tried to reduces the numbers of operations
+#Instructions
 
+- To compile use make command
+- ARG=$(shuf -i 0-500 -n 100 | tr '\n' ' '); ./push_swap $ARG | wc -l for testing
 
-
+#Resources
+## Links
+- https://medium.com/@jamierobertdawson/push-swap-the-least-amount-of-moves-with-two-stacks-d1e76a71789a
+## AI
+- To reduce lines of functions
+- Help visualize the chunk system i used
--- sort2.c
+++ sort2.c
@@ -6,130 +6,61 @@
 /*   By: sqrhead <sqrhead@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 10:48:25 by fshelna           #+#    #+#             */
-/*   Updated: 2026/01/18 17:41:55 by sqrhead          ###   ########.fr       */
+/*   Updated: 2026/01/18 19:55:49 by sqrhead          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 void	sort_low(t_stack *stacka, t_stack *stackb, int n_elements)
 {
 	if (n_elements <= 3)
-			sort_three(&stacka);
+		sort_three(&stacka);
 	else
-			sort_five(&stacka, &stackb);
+		sort_five(&stacka, &stackb);
 }
 
-int	find_in_chunk_position(t_stack *stack, t_stack_node *node)
+void	push_to_b(t_stack *sa, t_stack *sb, int chunksize)
 {
-	t_stack_node	*tmp;
-	int				index;
+	int	i;
 
-	if (!stack || !stack->node || !node)
-		return (-1);
-	tmp = stack->node;
-	index = 0;
-	while (tmp)
+	i = 0;
+	while (stack_get_len(sa) > 0)
 	{
-		if (tmp == node)
-			return (index);
-		index ++;
-		tmp = tmp->next;
-	}
-	return (-1);
-}
-
-t_stack_node	*find_node_by_nchunk(t_stack *stack, int chunk_index)
-{
-	t_stack_node *node;
-
-	if (!stack || !stack->node)
-		return (NULL);
-
-	node = stack->node;
-	while (node)
-	{
-		if (node->chunk_n == (size_t)chunk_index)
-			return (node);
-		node = node->next;
-	}
-	return (NULL);
-}
-
-t_stack_node	*node_get_closest(t_stack *stack, size_t index)
-{
-	t_stack_node	*node;
-	t_stack_node	*lnode;
-	int counter;
-
-	counter = 0;
-	if (!stack || !stack->node)
-		return (NULL);
-	node = stack->node;
-	lnode = get_node(stack, stack_get_len(stack) - counter  - 1);
-	while (lnode != node || node || lnode)
-	{
-		if (node->index == index)
-			return (node);
-		if (lnode->index == index)
-			return (lnode);
-		node = node->next;
-		counter ++;
-		lnode = get_node(stack, stack_get_len(stack) - counter - 1);
-	}
-	return (NULL);
-}
-
-
-void sort_high(t_stack *sa, t_stack *sb, int chunksize)
-{
-    int	i = 0;
-
-    while (stack_get_len(sa) > 0)
-    {
-        if (sa->node->index <= (size_t)i)
-        {
-            push_b(sa, sb);
-            // Optimization: If it's in the lower half of the range,
-            // rotate it to the bottom of B to keep B somewhat sorted.
+		if (sa->node->index <= (size_t)i)
+		{
+			push_b(sa, sb);
 			if (stack_get_len(sb) > 1)
-            	rotate_b(sb);
-            i++;
-        }
-        else if (sa->node->index <= (size_t)(i + chunksize))
-        {
-            push_b(sa, sb);
-            i++;
-        }
-        else
-        {
-            // Find the most efficient way to get a number from the range to the top
-            // Usually, a simple rotation works here, but a "find_closest" logic
-            // is even better for the move count.
-            rotate_a(sa);
-        }
-    }
+				rotate_b(sb);
+			i++;
+		}
+		else if (sa->node->index <= (size_t)(i + chunksize))
+		{
+			push_b(sa, sb);
+			i++;
+		}
+		else
+			rotate_a(sa);
+	}
+}
+void	sort_high(t_stack *sa, t_stack *sb, int chunksize)
+{
+	int	target;
+	int	pos;
+	int	size;
 
-    // Phase 2: Push from B back to A
-    // We always look for the largest remaining number to keep A sorted.
-    while (stack_get_len(sb) > 0)
-    {
-        int target = stack_get_len(sb) - 1;
-        int pos = get_index(sb, target);
-        int size = stack_get_len(sb);
-
-        // Bring the target (max) to the top of B
-        if (pos <= size / 2)
-        {
-            while ((int)sb->node->index != target)
-                rotate_b(sb);
-        }
-        else
-        {
-            while ((int)sb->node->index != target)
-                reverse_rotate_b(sb);
-        }
-        push_a(sa, sb);
-    }
+	push_to_b(sa, sb, chunksize);
+	while (stack_get_len(sb) > 0)
+	{
+		target = stack_get_len(sb) - 1;
+		pos = get_index(sb, target);
+		size = stack_get_len(sb);
+		if (pos <= size / 2)
+			while ((int)sb->node->index != target)
+				rotate_b(sb);
+		else
+			while ((int)sb->node->index != target)
+				reverse_rotate_b(sb);
+		push_a(sa, sb);
+	}
 }
